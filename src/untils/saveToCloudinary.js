2c228d58026a3c59697a6c6b2a175edcbae30021
kefllr <timislusar05@gmail.com>
2024-07-05 19:41:00 +0200

    nodejs-hw-mongodb

--- src/untils/saveToCloudinary.js
+++ src/untils/saveToCloudinary.js
@@ -3,7 +3,7 @@ import { env } from './env.js';
 import { ENV_VARS } from '../constans/index.js';
 import fs from "node:fs/promises";
 
-cloudinary.config({ 
+cloudinary.v2.config({ 
     cloud_name: env(ENV_VARS.CLOUDINARY_NAME), 
     api_key: env(ENV_VARS.CLOUDINARY_API_KEY), 
     api_secret:  env(ENV_VARS.CLOUDINARY_API_SECRET)
@@ -11,7 +11,7 @@ cloudinary.config({
 
 export const saveToCloudinary = async (file) =>{
 
-    const res = await cloudinary.uploader.upload(file.path); 
+    const res = await cloudinary.v2.uploader.upload(file.path); 
     await fs.unlink(file.path);
     return res.secure_url;
 };
