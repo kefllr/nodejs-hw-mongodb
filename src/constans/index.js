2f345781bf2378575ece8769a5d913bc74cfe39a
kefllr <timislusar05@gmail.com>
2024-07-05 20:25:35 +0200

    nodejs-hw-mongodb

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
+import path from "node:path";
 
 export const ENV_VARS = {
     PORT: 'PORT',
@@ -19,8 +17,8 @@ export const ENV_VARS = {
     CLOUDINARY_API_KEY: 'CLOUDINARY_API_KEY',
     CLOUDINARY_API_SECRET: 'CLOUDINARY_API_SECRET',
     IS_CLOUDINARY_ENABLED: 'IS_CLOUDINARY_ENABLED'
-};
+  };
 
-export const TEMPLATE_DIR = 'src/templates';
-export const TEMP_UPLOAD_DIR = 'temp';
-export const UPLOAD_DIR = 'upload';
+  export const TEMPLATE_DIR = path.join(process.cwd(), 'src', 'templates');
+  export const  TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+  export const  UPLOAD_DIR = path.join(process.cwd(), 'upload');
